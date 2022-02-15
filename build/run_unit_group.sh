a86eee558f3d5946f13a91d3ce80f69cd381502c
Nicolò Boschi <boschi1997@gmail.com>
2022-02-15 19:06:52 +0100

    Jacoco - Codecov.io integration

--- build/run_unit_group.sh
+++ build/run_unit_group.sh
@@ -24,7 +24,7 @@ set -e
 set -o pipefail
 set -o errexit
 
-MVN_TEST_OPTIONS='mvn -B -ntp -DskipSourceReleaseAssembly=true -DskipBuildDistribution=true -Dspotbugs.skip=true -Dlicense.skip=true -Dcheckstyle.skip=true -Drat.skip=true'
+MVN_TEST_OPTIONS='mvn -B -Pmain,coverage -ntp -DskipSourceReleaseAssembly=true -DskipBuildDistribution=true -Dspotbugs.skip=true -Dlicense.skip=true -Dcheckstyle.skip=true -Drat.skip=true'
 
 function mvn_test() {
   (
@@ -132,6 +132,7 @@ function test_group_proxy() {
     echo "::endgroup::"
 }
 
+<<<<<<< HEAD
 function test_group_other() {
   mvn_test --install -PbrokerSkipTest \
            -Dexclude='org/apache/pulsar/proxy/**/*.java,
@@ -139,6 +140,16 @@ function test_group_other() {
                   **/TestPulsarKeyValueSchemaHandler.java,
                   **/PrimitiveSchemaTest.java,
                   BlobStoreManagedLedgerOffloaderTest.java'
+=======
+function other() {
+  $MVN_COMMAND_WITH_RETRY clean install -PbrokerSkipTest,coverage \
+                                     -Dexclude='org/apache/pulsar/proxy/**/*.java,
+                                                **/ManagedLedgerTest.java,
+                                                **/TestPulsarKeyValueSchemaHandler.java,
+                                                **/PrimitiveSchemaTest.java,
+                                                BlobStoreManagedLedgerOffloaderTest.java,
+                                                **/ManagedLedgerFactoryShutdownTest.java'
+>>>>>>> d81860fbc2c (Jacoco - Codecov.io integration)
 
   mvn_test -pl managed-ledger -Dinclude='**/ManagedLedgerTest.java,
                                                   **/OffloadersCacheTest.java'
