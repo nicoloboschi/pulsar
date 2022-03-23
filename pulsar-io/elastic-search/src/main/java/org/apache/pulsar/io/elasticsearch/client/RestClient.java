591e75ad909e5e8193135dbcabcaa466d994d197
Nicolò Boschi <boschi1997@gmail.com>
2022-03-23 15:45:03 +0100

    [elasticsearch] support token based auth

--- pulsar-io/elastic-search/src/main/java/org/apache/pulsar/io/elasticsearch/client/RestClient.java
+++ pulsar-io/elastic-search/src/main/java/org/apache/pulsar/io/elasticsearch/client/RestClient.java
@@ -20,6 +20,8 @@ package org.apache.pulsar.io.elasticsearch.client;
 
 import com.google.common.base.Strings;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
@@ -32,6 +34,7 @@ import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
 import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
 import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.nio.conn.NHttpClientConnectionManager;
 import org.apache.http.nio.conn.NoopIOSessionStrategy;
 import org.apache.http.nio.conn.SchemeIOSessionStrategy;
@@ -56,6 +59,9 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -98,10 +104,12 @@ public abstract class RestClient implements Closeable {
             org.opensearch.client.RestClientBuilder.HttpClientConfigCallback {
         final NHttpClientConnectionManager connectionManager;
         final CredentialsProvider credentialsProvider;
+        final List<Header> defaultHeaders;
 
         public ConfigCallback() {
             this.connectionManager = buildConnectionManager(RestClient.this.config);
             this.credentialsProvider = buildCredentialsProvider(RestClient.this.config);
+            this.defaultHeaders = buildDefaultHeaders(RestClient.this.config);
         }
 
         @Override
@@ -109,9 +117,12 @@ public abstract class RestClient implements Closeable {
             builder.setMaxConnPerRoute(config.getBulkConcurrentRequests());
             builder.setMaxConnTotal(config.getBulkConcurrentRequests());
             builder.setConnectionManager(connectionManager);
-
             if (this.credentialsProvider != null) {
                 builder.setDefaultCredentialsProvider(credentialsProvider);
+            }
+            if (defaultHeaders != null) {
+                System.out.println("setting defaultHeaders " + defaultHeaders.get(0).getName() + ": " + defaultHeaders.get(0).getValue());
+                builder.setDefaultHeaders(defaultHeaders);
             }
             return builder;
         }
@@ -183,6 +194,22 @@ public abstract class RestClient implements Closeable {
             credentialsProvider.setCredentials(AuthScope.ANY,
                     new UsernamePasswordCredentials(config.getUsername(), config.getPassword()));
             return credentialsProvider;
+        }
+
+        private List<Header> buildDefaultHeaders(ElasticSearchConfig config) {
+            System.out.println("buildDefaultHeaders" + config);
+            if (StringUtils.isEmpty(config.getToken()) && StringUtils.isEmpty(config.getApiKey())) {
+                return null;
+            }
+            List<Header> headers = new ArrayList<>();
+            String authHeaderValue;
+            if (!StringUtils.isEmpty(config.getToken())) {
+                authHeaderValue = "Bearer " + config.getToken();
+            } else {
+                authHeaderValue = "ApiKey " + config.getApiKey();
+            }
+            headers.add(new BasicHeader(HttpHeaders.AUTHORIZATION, authHeaderValue));
+            return Collections.unmodifiableList(headers);
         }
     }
 
